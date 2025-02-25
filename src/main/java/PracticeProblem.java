3b3c63e378797621c867bda1318dbbc6f6928bdb
marcus-wong-ics4u1-02-25 <marcus.wong5@student.tdsb.on.ca>
2025-02-25 19:09:33 +0000

    same assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,4 +3,39 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static boolean isPalindrome(String word) {
+		word = word.replaceAll("\\s+", "").toLowerCase();
+		for (int i = 0, j = word.length() - 1; i < j; i++, j--) {
+			if (word.charAt(i) != word.charAt(j)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static int getAge(String[] names, int[] ages, String name) {
+		for (int i = 0; i < names.length; i++) {
+			if (names[i].equals(name)) {
+				return ages[i];
+		}
+		
+		}
+		return -1;
+
+	}
+	public static int countWords(String word, char c) {
+		String[] wordArray = word.split(" ");
+		int count = 0;
+		for (int i = 0; i < wordArray.length; i++) {
+			if (wordArray[i].indexOf(c) != -1) {
+				count++;
+
+			}
+
+		}
+		return count;
+
+
+	}
+	
 }
